a7b3f4a76ec9ea6154012c4afb2b89106ebf72c9
Padhma <padhmasahithya@gmail.com>
2019-04-24 00:02:18 +0530

    Changes respective to output

--- crawler/crawler/spiders/MarketStats.py
+++ crawler/crawler/spiders/MarketStats.py
@@ -4,13 +4,6 @@ from scrapy_splash import SplashRequest
 class MarketstatsSpider(scrapy.Spider):
     name = 'MarketStats'
     allowed_domains = ['www.economictimes.indiatimes.com']
-    # start_urls = [
-    #     'https://economictimes.indiatimes.com/marketstats/pageno-1,pid-59,sortby-currentYearRank,sortorder-asc,year-2018.cms',
-    #     'https://economictimes.indiatimes.com/marketstats/pid-58,pageno-1,year-2017,sortorder-asc,sortby-CurrentYearRank.cms',
-    #     'https://economictimes.indiatimes.com/marketstats/pid-57,pageno-1,year-2016,sortorder-asc,sortby-CurrentYearRank.cms',
-    #     'https://economictimes.indiatimes.com/marketstats/pid-56,pageno-1,year-2015,sortorder-asc,sortby-CurrentYearRank.cms',
-    #     'https://economictimes.indiatimes.com/marketstats/pid-55,pageno-1,year-2014,sortorder-asc,sortby-CurrentYearRank.cms',
-    # ]
 
     def start_requests(self):
         urls = [
@@ -22,10 +15,6 @@ class MarketstatsSpider(scrapy.Spider):
         ]
         for url in urls:
             yield SplashRequest(url, self.parse, args={'wait': 3})
-        # yield SplashRequest(
-        #     url = 'https://economictimes.indiatimes.com/marketstats/pageno-1,pid-59,sortby-currentYearRank,sortorder-asc,year-2018.cms',
-        #     callback = self.parse,
-        # )
 
     def parse(self, response):
         for stats in response.xpath("//div[@class='dataList']"):
@@ -36,5 +25,3 @@ class MarketstatsSpider(scrapy.Spider):
                 'PAT': stats.css(".dataList ul>li:nth-child(n+6)::text").extract_first(),
                 'MCAP': stats.css(".dataList ul>li:nth-child(n+8)::text").extract_first(),
             }
-        # nextLinkSelector = response.css('#e_ET_500_Companies_2017::attr("href")').extract()
-        # yield scrapy.Request(urls=response.urljoin(nextLinkSelector))
